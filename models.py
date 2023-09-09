87ad4bf560a6fcb850204979b832aba3335394e1
IvDmAl <144175574+IvDmAl@users.noreply.github.com>
2023-09-09 15:57:06 +0300

    добавляю возможность взять данные из таблицы

--- models.py
+++ models.py
@@ -8,3 +8,21 @@ class UserProfile:
     group: str
     description: str
 
+@dataclass
+class UserData:
+    id: int
+    name: str
+    age: int
+    group: str
+    description: str
+    isActive: int
+@dataclass
+class EventData:
+    id: int
+    time_start : str
+    time_end : str
+    description : str
+@dataclass
+class ImageData:
+    id: int
+    path : str
